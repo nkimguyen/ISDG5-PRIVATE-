c40db45ae017971ee80f5102003c17683e6789a4
Kim Nguyen <kim.nguyen-4@student.uts.edu.au>
2026-03-25 11:06:55 +1100

    db table sample created

---
+++ backend/db_init.py
@@ -0,0 +1,19 @@
+from backend.models.db_db_connect import get_db_connection
+
+def init_db():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        customer_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        first_name TEXT NOT NULL,
+        last_name TEXT NOT NULL,
+        email TEXT NOT NULL UNIQUE,
+        password TEXT NOT NULL,
+        phone_number TEXT,
+        address TEXT
+        status TEXT DEFAULT 'active'
+    )
+""")
+conn.commit()
