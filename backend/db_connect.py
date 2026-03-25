81f4350ad01f86029a7fc9668f2f9ae6cea2b6f9
Kim Nguyen <kim.nguyen-4@student.uts.edu.au>
2026-03-25 11:26:32 +1100

    sample backend init

--- backend/db_connect.py
+++ backend/db_connect.py
@@ -9,4 +9,4 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
     return conn
 
-get_db_connection()
+# get_db_connection()
--- backend/db_init.py
+++ backend/db_init.py
@@ -1,19 +1,31 @@
-from backend.models.db_db_connect import get_db_connection
+from backend.db_connect import get_db_connection
 
 def init_db():
     conn = get_db_connection()
     cursor = conn.cursor()
 
-cursor.execute("""
-    CREATE TABLE IF NOT EXISTS users (
-        customer_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        first_name TEXT NOT NULL,
-        last_name TEXT NOT NULL,
-        email TEXT NOT NULL UNIQUE,
-        password TEXT NOT NULL,
-        phone_number TEXT,
-        address TEXT
-        status TEXT DEFAULT 'active'
-    )
-""")
-conn.commit()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            customer_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            first_name TEXT NOT NULL,
+            last_name TEXT NOT NULL,
+            email TEXT NOT NULL UNIQUE,
+            password TEXT NOT NULL,
+            phone_number TEXT,
+            address TEXT
+            status TEXT DEFAULT 'active'
+        )
+    """)
+    conn.commit()
+
+    print("Table created")
+
+    cursor.execute("""
+        INSERT INTO users (first_name, last_name, email, password, phone_number, address)
+        VALUES (?, ?, ?, ?, ?, ?)
+    """, ("John", "Doe", "john.doe@example.com", "password123", "123-456-7890", "123 Main St"))
+    conn.commit()
+    print("Sample user inserted")
+    conn.close()
+
+init_db()
